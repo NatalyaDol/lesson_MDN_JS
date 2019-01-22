c9b3d1a619c1a985573cdb84b9bef73093ceb67c
NatalyaDol <suhoikova@gmail.com>
2019-01-22 12:15:40 +0300

    Add function get checked in checkbox

---
+++ beginner-html-site-styled-gh-pages/scripts/checkbox.js
@@ -0,0 +1,9 @@
+function getChecked() {
+    var checkbox = document.getElementById('one');
+
+    if (checkbox.checked) {
+        alert('Выбран');
+    } else {
+        alert('Не выбран');
+    }
+}
